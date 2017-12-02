8349f16ef4a29d5862b8def9b3f49058d396e973
sulancheng <suchengkuaile@126.com>
2017-12-02 12:48:37 +0800

    1,修改了加密的工具 2.在书签中加了 搜索栏中有的历史记录的不同长度的标签 button.

---
+++ app/src/main/java/com/susu/hh/mygreendao/Recycleviewactivity.java
@@ -0,0 +1,13 @@
+package com.susu.hh.mygreendao;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Recycleviewactivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycleviewactivity);
+    }
+}
--- app/src/main/java/com/susu/hh/mygreendao/utils/Base64Utils.java
+++ app/src/main/java/com/susu/hh/mygreendao/utils/Base64Utils.java
@@ -2,6 +2,9 @@ package com.susu.hh.mygreendao.utils;
 
 import android.util.Base64;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.UnsupportedEncodingException;
 
 /**
@@ -33,5 +36,35 @@ public class Base64Utils {
             }
         }
         return result;
+    }
+    //）对文件进行Base64编码
+    public String jiamwenjian(){
+        String encodedString = "";
+        File file = new File("/storage/emulated/0/pimsecure_debug.txt");
+        FileInputStream inputFile = null;
+        try {
+            inputFile = new FileInputStream(file);
+            byte[] buffer = new byte[(int) file.length()];
+            inputFile.read(buffer);
+            inputFile.close();
+            encodedString= Base64.encodeToString(buffer, Base64.DEFAULT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encodedString;
+    }
+    //4.）对文件进行Base64解码
+    public void jiemwenjian(){
+        String encodedString = "";
+        File desFile = new File("/storage/emulated/0/pimsecure_debug_1.txt");
+        FileOutputStream fos = null;
+        try {
+            byte[] decodeBytes = Base64.decode(encodedString.getBytes(), Base64.DEFAULT);
+            fos = new FileOutputStream(desFile);
+            fos.write(decodeBytes);
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
